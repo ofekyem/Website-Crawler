02989fe7e17b28bb3bd43bb1a1106e0400a0b8b5
Ofek Yemini <ofekyemini87@gmail.com>
2024-10-22 00:28:58 +0300

    crawl page works for multiple pages

--- crawl.js
+++ crawl.js
@@ -1,25 +1,46 @@
 const {JSDOM} = require('jsdom')
 
-async function crawlPage(currentURL){
+async function crawlPage(baseURL, currentURL, pages){ 
+
+    const baseUrlObj = new URL(baseURL) 
+    const currentURLObj = new URL(currentURL) 
+    if(baseUrlObj.hostname !== currentURLObj.hostname){
+        return pages
+    } 
+    const normalizedCurrentURL = normalizeURL(currentURL) 
+    if(pages[normalizedCurrentURL]>0){
+        pages[normalizedCurrentURL]++ 
+        return pages
+    } 
+
+    pages[normalizedCurrentURL]=1 
+
     console.log(`Actively crawling the page of: ${currentURL}`) 
+
     try{
         const resp = await fetch(currentURL) 
         if(resp.status>=400){
             console.log(`problem with fetch status code: ${resp.status} on page: ${currentURL}`)
-            return
+            return pages
         } 
         const contentType= resp.headers.get("content-type")
         if(!contentType.includes("text/html")){
             console.log(`non html response, content type: ${contentType}, on page: ${currentURL}`)
-            return
-        }
+            return pages
+        } 
+        const HTMLBody = await resp.text() 
+        const nextURLs = getURLsFromHTML(HTMLBody,baseURL) 
 
+        for(const url of nextURLs){ 
+            pages = await crawlPage(baseURL,url,pages)
+
+        }
 
     } catch(err){
         console.log(`problem in fetch action: ${err.message}, on page ${currentURL}`)
-    }
+    } 
 
-    
+    return pages
 
 }
 
